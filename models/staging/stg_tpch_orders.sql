d77e8283c9444a43c225095600d5a3e6c6077ec2
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2025-01-08 15:36:43 +0800

    fix typo

--- models/staging/stg_tpch_orders.sql
+++ models/staging/stg_tpch_orders.sql
@@ -1,6 +1,6 @@
 select
     o_orderkey as order_key,
-    o_custkey as o_custkey,
+    o_custkey as customer_key,
     o_orderstatus as status_code,
     o_totalprice as total_price,
     o_orderdate as order_date,
